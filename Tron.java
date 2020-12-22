f5840618cb51590141857b6d5eacde1a12c1941b
LucasVaurie <lucas.vaurie91@gmail.com>
2020-12-22 14:49:32 +0100

    Tron V0.1.1 - Mappage de la grille - Non fonct.
    Ajout de l'affichage initial de la grille sur la console.
    Ajout de la modif. de la grille en cours de partie (num. joueur dans la grille)
    ___
    Débug : Initialisation grille hors de la game loop

--- Tron.java
+++ Tron.java
@@ -10,19 +10,19 @@ class Tron {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-
+        char [][] grille = new char[30][20];
+        // Initialisation à 0 de la grille de jeu
+        for (int boucleur_1 = 0 ; boucleur_1 < 30 ; boucleur_1 ++) {
+            System.err.println(); //debug
+            for (int boucleur_2 = 0 ; boucleur_2 < 20 ; boucleur_2 ++) {
+                grille[boucleur_1][boucleur_2] = 'o';
+                System.err.print(grille[boucleur_1][boucleur_2] + " "); //debug
+            }
+        }
         // game loop
         while (true) {
             int N = in.nextInt(); // total number of players (2 to 4).
             int P = in.nextInt(); // your player number (0 to 3).
-            
-            char [][] grille = new char[30][20];
-            for (int boucleur_1 = 0 ; boucleur_1 < 30 ; boucleur_1 ++) {
-                for (int boucleur_2 = 0 ; boucleur_2 < 20 ; boucleur_2 ++) {
-                    grille[boucleur_1][boucleur_2] = 'o';
-
-                }
-            }
 
             //System.err.println(Arrays.toString(grille));
             
@@ -31,6 +31,7 @@ class Tron {
                 int Y0 = in.nextInt(); // starting Y coordinate of lightcycle (or -1)
                 int X1 = in.nextInt(); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
                 int Y1 = in.nextInt(); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
+                grille[X1][Y1] = (char)P;
             }
 
             // Write an action using System.out.println()
