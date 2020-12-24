ae75af007d933a730fd11e183822dc8fea67fc25
LucasVaurie <lucas.vaurie91@gmail.com>
2020-12-24 10:26:52 +0100

    Tron V1.0 - Fonctionnel - Ligue Bois 1 (1er)
    Première version fonctionnelle de Tron
    Ligue Bois 1 (1ère place, ne bat pas le boss)

--- Tron.java
+++ Tron.java
@@ -11,20 +11,19 @@ class Player {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         char [][] grille = new char[20][30];
-        // Initialisation à 0 de la grille de jeu
+        
+        // Initialisation de la grille de jeu à 0
         for (int boucleur_1 = 0 ; boucleur_1 < 20 ; boucleur_1 ++) {
-            //System.err.println(); //debug
             for (int boucleur_2 = 0 ; boucleur_2 < 30 ; boucleur_2 ++) {
                 grille[boucleur_1][boucleur_2] = 'o';
-                //System.err.print(grille[boucleur_1][boucleur_2] + " "); //debug
             }
         }
         // game loop
         while (true) {
             int N = in.nextInt(); // total number of players (2 to 4).
             int P = in.nextInt(); // your player number (0 to 3).
-
-            //System.err.println(Arrays.toString(grille));
+            int currentX = 0, currentY = 0;
+            boolean enCours = true;
             
             for (int i = 0; i < N; i++) {
                 int X0 = in.nextInt(); // starting X coordinate of lightcycle (or -1)
@@ -32,21 +31,45 @@ class Player {
                 int X1 = in.nextInt(); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
                 int Y1 = in.nextInt(); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
                 grille[Y1][X1] = Character.forDigit(i, 16);
+                if (i == P) {
+                    currentX = X1;
+                    currentY = Y1;
+                }
             }
 
-            
+            /* // AFFICHAGE GRILLE DE JEU
             for (int i = 0 ; i < 20 ; i ++) {
-                for (int y = 0 ; y < 30 ; y ++) {
+                for (int y = 0 ; y < 30 ; y ++)
                     System.err.print(grille[i][y] + " "); //debug
-                }
                 System.err.println(); //debug
             }
-            
+            */
 
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
-
-            System.out.println("LEFT"); // A single line with UP, DOWN, LEFT or RIGHT
+            if (currentX-1 != -1 && enCours) {
+                if (grille[currentY][currentX-1] == 'o') {
+                    System.out.println("LEFT");
+                    enCours = false;
+                }
+            }
+            if (currentY-1 != -1 && enCours) {   
+                if (grille[currentY-1][currentX] == 'o') {
+                    System.out.println("UP");
+                    enCours = false;
+                }
+            }
+            if (currentX+1 <= 29 && enCours) {
+                if (grille[currentY][currentX+1] == 'o') {
+                    System.out.println("RIGHT");
+                    enCours = false;
+                }
+            }
+            if (currentY+1 <= 19 && enCours){
+                if (grille[currentY+1][currentX] == 'o') {
+                    System.out.println("DOWN");
+                    enCours = false;
+                }
+            }
+        
         }
     }
 }
