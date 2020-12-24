8c44fa7c0107924043ed5818f8c9828dbebd20ee
LucasVaurie <lucas.vaurie91@gmail.com>
2020-12-24 14:26:38 +0100

    Tron V1.1 - Ajout fonction actionChoisieFonct. - Ligue Bois 1 (1er)
    Séparation de la partie de déplacement du bot en une fonction distincte.
    
    Toujours classé 1er de la ligue bois

--- Tron.java
+++ Tron.java
@@ -2,17 +2,52 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-class Player {
+class Tron {
+    public static String actionChoisie(char[][] grilleJeu, int currentX, int currentY) {
+        boolean enCours = true;
+        // Gestion de 'Aller à gauche'
+        if (currentX-1 != -1 && enCours) {
+            if (grilleJeu[currentY][currentX-1] == 'o') {
+                enCours = false;
+                return "LEFT";
+            }
+        }
+        // Gestion de 'Aller en haut'
+        if (currentY-1 != -1 && enCours) {   
+            if (grilleJeu[currentY-1][currentX] == 'o') {
+                enCours = false;
+                return "UP";
+            }
+        }
+        // Gestion de 'Aller en bas'
+        if (currentY+1 <= 19 && enCours){
+            if (grilleJeu[currentY+1][currentX] == 'o') {
+                enCours = false;
+                return "DOWN";
+            }
+        }
+        // Gestion de 'Aller à droite'
+        if (currentX+1 <= 29 && enCours) {
+            if (grilleJeu[currentY][currentX+1] == 'o') {
+                enCours = false;
+                return "RIGHT";
+            }
+        }
+        return "null";
+    }
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         char [][] grille = new char[20][30];
-        
-        // Initialisation de la grille de jeu à 0
+        // Initialisation à 0 de la grille de jeu
         for (int boucleur_1 = 0 ; boucleur_1 < 20 ; boucleur_1 ++) {
             for (int boucleur_2 = 0 ; boucleur_2 < 30 ; boucleur_2 ++) {
                 grille[boucleur_1][boucleur_2] = 'o';
@@ -23,7 +58,7 @@ class Player {
             int N = in.nextInt(); // total number of players (2 to 4).
             int P = in.nextInt(); // your player number (0 to 3).
             int currentX = 0, currentY = 0;
-            boolean enCours = true;
+            // boolean enCours = true;
             
             for (int i = 0; i < N; i++) {
                 int X0 = in.nextInt(); // starting X coordinate of lightcycle (or -1)
@@ -36,40 +71,8 @@ class Player {
                     currentY = Y1;
                 }
             }
-
-            /* // AFFICHAGE GRILLE DE JEU
-            for (int i = 0 ; i < 20 ; i ++) {
-                for (int y = 0 ; y < 30 ; y ++)
-                    System.err.print(grille[i][y] + " "); //debug
-                System.err.println(); //debug
-            }
-            */
-
-            if (currentX-1 != -1 && enCours) {
-                if (grille[currentY][currentX-1] == 'o') {
-                    System.out.println("LEFT");
-                    enCours = false;
-                }
-            }
-            if (currentY-1 != -1 && enCours) {   
-                if (grille[currentY-1][currentX] == 'o') {
-                    System.out.println("UP");
-                    enCours = false;
-                }
-            }
-            if (currentX+1 <= 29 && enCours) {
-                if (grille[currentY][currentX+1] == 'o') {
-                    System.out.println("RIGHT");
-                    enCours = false;
-                }
-            }
-            if (currentY+1 <= 19 && enCours){
-                if (grille[currentY+1][currentX] == 'o') {
-                    System.out.println("DOWN");
-                    enCours = false;
-                }
-            }
-        
+            System.out.println(actionChoisie(grille, currentX, currentY));
         }
     }
+    
 }
