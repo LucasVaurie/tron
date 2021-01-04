f42576baffccd5f1945d4e8491cdc2c44261d870
LucasVaurie <lucas.vaurie91@gmail.com>
2021-01-04 15:59:53 +0100

    Tron V2.1 - Fonctionnel - Ligue Bronze (550ème) … fe9a4e7
    Compartimentage du code en fonctions distinctes

--- Tron.java
+++ Tron.java
@@ -6,92 +6,17 @@ import java.math.*;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-class Tron {
-    public static String actionChoisie(char[][] grilleJeu, int currentX, int currentY, String direction) {
-        boolean enCours = true;
-        
-        if (direction == "LEFT") {
-            if (currentX-1 != -1 && enCours) {
-                if (grilleJeu[currentY][currentX-1] == 'o') {
-                    enCours = false;
-                    return "LEFT";
-                }
-            }
-        } else if (direction == "UP") {
-            if (currentY-1 != -1 && enCours) {   
-                if (grilleJeu[currentY-1][currentX] == 'o') {
-                    enCours = false;
-                    return "UP";
-                }
-            }
-        } else if (direction == "DOWN") {
-            if (currentY+1 <= 19 && enCours){
-                if (grilleJeu[currentY+1][currentX] == 'o') {
-                    enCours = false;
-                    return "DOWN";
-                }
-            }
-        } else if (direction == "RIGHT") {
-            if (currentX+1 <= 29 && enCours) {
-                if (grilleJeu[currentY][currentX+1] == 'o') {
-                    enCours = false;
-                    return "RIGHT";
-                }
-            }
-        }
-
-        
-        // Gestion de 'Aller à gauche'
-        if (currentX-1 != -1 && enCours) {
-            if (grilleJeu[currentY][currentX-1] == 'o') {
-                enCours = false;
-                return "LEFT";
-            }
-        }
-
-        // Gestion de 'Aller en haut'
-        if (currentY-1 != -1 && enCours) {   
-            if (grilleJeu[currentY-1][currentX] == 'o') {
-                enCours = false;
-                return "UP";
-            }
-        }
-
-        // Gestion de 'Aller en bas'
-        if (currentY+1 <= 19 && enCours){
-            if (grilleJeu[currentY+1][currentX] == 'o') {
-                enCours = false;
-                return "DOWN";
-            }
-        }
-
-        // Gestion de 'Aller à droite'
-        if (currentX+1 <= 29 && enCours) {
-            if (grilleJeu[currentY][currentX+1] == 'o') {
-                enCours = false;
-                return "RIGHT";
-            }
-        }
-        return "null";
-    }
-
+class Player {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         char [][] grille = new char[20][30];
-        String direction = "null";
-        // Initialisation à 0 de la grille de jeu
-        for (int boucleur_1 = 0 ; boucleur_1 < 20 ; boucleur_1 ++) {
-            for (int boucleur_2 = 0 ; boucleur_2 < 30 ; boucleur_2 ++) {
-                grille[boucleur_1][boucleur_2] = 'o';
-            }
-        }
+        grille = initGrille(grille); // Initialisation à 0 de la grille de jeu
+        String direction = "null";    
         // game loop
         while (true) {
             int N = in.nextInt(); // total number of players (2 to 4).
             int P = in.nextInt(); // your player number (0 to 3).
             int currentX = 0, currentY = 0;
-            // boolean enCours = true;
-            
             for (int i = 0; i < N; i++) {
                 int X0 = in.nextInt(); // starting X coordinate of lightcycle (or -1)
                 int Y0 = in.nextInt(); // starting Y coordinate of lightcycle (or -1)
@@ -104,10 +29,101 @@ class Tron {
                     currentY = Y1;
                 }
             }
-
             direction = actionChoisie(grille, currentX, currentY, direction);
             System.out.println(direction);
         }
     }
+
+    public static char[][] initGrille(char[][] grilleVide) {
+        for (int i = 0 ; i < 20 ; i ++) {
+            for (int j = 0 ; j < 30 ; j ++) {
+                grilleVide[i][j] = 'o';
+            }
+        }
+        return grilleVide;
+    }
     
+    public static String actionChoisie(char[][] grilleJeu, int currentX, int currentY, String direction) {
+        boolean enCours = true;
+        String decision = "null";
+        if (direction != "null") {
+            decision = maintienDirection(grilleJeu, currentX, currentY, enCours, direction);
+            if (decision != "null")
+                return decision;
+        }
+        // Gestion de 'Aller à gauche'
+        if (currentX-1 != -1 && enCours) {
+            decision = dirGauche(grilleJeu, currentX, currentY, enCours);
+            if (decision != "null")
+                return decision;
+        }
+        // Gestion de 'Aller en haut'
+        if (currentY-1 != -1 && enCours) {   
+            decision = dirHaut(grilleJeu, currentX, currentY, enCours);
+            if (decision != "null")
+                return decision;
+        }
+        // Gestion de 'Aller en bas'
+        if (currentY+1 <= 19 && enCours){
+            decision = dirBas(grilleJeu, currentX, currentY, enCours);
+            if (decision != "null")
+                return decision;
+        }
+        // Gestion de 'Aller à droite'
+        if (currentX+1 <= 29 && enCours) {
+            decision = dirDroite(grilleJeu, currentX, currentY, enCours);
+            if (decision != "null")
+                return decision;
+        }
+        return "null";
+    }
+
+    public static String maintienDirection(char[][] currentGrille, int currentX, int currentY, boolean running, String dir) {
+        if (dir == "LEFT") {
+            if (currentX-1 != -1 && running)
+                return dirGauche(currentGrille, currentX, currentY, running);
+        } else if (dir == "UP") {
+            if (currentY-1 != -1 && running)   
+                return dirHaut(currentGrille, currentX, currentY, running);
+        } else if (dir == "DOWN") {
+            if (currentY+1 <= 19 && running)
+                return dirBas(currentGrille, currentX, currentY, running);
+        } else if (dir == "RIGHT") {
+            if (currentX+1 <= 29 && running)
+                return dirDroite(currentGrille, currentX, currentY, running);
+        }
+        return "null";
+    }
+
+    public static String dirGauche(char[][] currentGrille, int currentX, int currentY, boolean running) {
+        if (currentGrille[currentY][currentX-1] == 'o') {
+            running = false;
+            return "LEFT";
+        } 
+        return "null";  
+    }
+
+    public static String dirDroite(char[][] currentGrille, int currentX, int currentY, boolean running) {
+        if (currentGrille[currentY][currentX+1] == 'o') {
+            running = false;
+            return "RIGHT";
+        } 
+        return "null";  
+    }
+
+    public static String dirHaut(char[][] currentGrille, int currentX, int currentY, boolean running) {
+        if (currentGrille[currentY-1][currentX] == 'o') {
+            running = false;
+            return "UP";
+        } 
+        return "null";  
+    }
+
+    public static String dirBas(char[][] currentGrille, int currentX, int currentY, boolean running) {
+        if (currentGrille[currentY+1][currentX] == 'o') {
+            running = false;
+            return "DOWN";
+        } 
+        return "null";  
+    }
 }
